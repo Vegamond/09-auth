109c9f3a88ae147e04bab0b98b75653a3b9c3214
Vega <alexkarpytskyi@gmail.com>
2026-04-26 20:42:35 +0300

    fix: NoteForm default import

--- app/(private routes)/notes/action/create/page.tsx
+++ app/(private routes)/notes/action/create/page.tsx
@@ -1,4 +1,4 @@
-import { NoteForm } from "@/components/NoteForm/NoteForm";
+import NoteForm from "@/components/NoteForm/NoteForm";
 import css from "./page.module.css";
 import type { Metadata } from "next";
 
@@ -29,6 +29,5 @@ export default function CreateNote() {
                 <NoteForm/>
             </div>
         </main>
-
     );
 }
