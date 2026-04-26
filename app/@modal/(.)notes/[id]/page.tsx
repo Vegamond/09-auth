e0cd9a7be52dd2fc092f95e4b63b96b69efca151
Vega <alexkarpytskyi@gmail.com>
2026-04-26 21:59:44 +0300

    fix: update page.tsx for modal with correct SSR prefetch

--- app/@modal/(.)notes/[id]/page.tsx
+++ app/@modal/(.)notes/[id]/page.tsx
@@ -1,10 +1,27 @@
-import NoteModal from '@/components/NoteModal/NoteModal';
+import { dehydrate, HydrationBoundary, QueryClient } from '@tanstack/react-query';
+// Шлях імпорту змінився, бо тепер файли лежать поруч в одній папці [id]
+import NotePreviewClient from './NotePreview.client'; 
+import { fetchNoteById } from '@/lib/api/serverApi';
 
-type Props = {
+interface NoteModalProps {
     params: Promise<{ id: string }>;
-};
+}
 
-export default async function NoteModalPage({ params }: Props) {
+// Назву функції краще змінити на Page, як це прийнято для роутів Next.js
+export default async function Page({ params }: NoteModalProps) {
     const { id } = await params;
-    return <NoteModal id={id} />;
+    const queryClient = new QueryClient();
+
+    // Завантажуємо дані нотатки на сервері
+    await queryClient.prefetchQuery({
+        queryKey: ['note', id],
+        queryFn: () => fetchNoteById(id),
+    });
+
+    return (
+        // Передаємо завантажені дані на клієнт (гідратація)
+        <HydrationBoundary state={dehydrate(queryClient)}>
+            <NotePreviewClient noteId={id} />
+        </HydrationBoundary>
+    );
 }
